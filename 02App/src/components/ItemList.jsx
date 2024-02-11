5c2411bf4e1a0f0ed905d2e4a650d8cd6c3a68b1
m-kazi <tanim703@gmail.com>
2024-02-11 19:58:29 +0600

    Added LineItems passed it from ItemList

--- 02App/src/components/ItemList.jsx
+++ 02App/src/components/ItemList.jsx
@@ -1,33 +1,16 @@
 /* eslint-disable react/prop-types */
-import { FaTrashAlt } from "react-icons/fa";
+import LineItems from "./LineItems";
 
 const ItemList = ({ items, handleCheck, handleDelete }) => {
     return (
         <ul>
             {items.map((item) => (
-                <li className="item" key={item.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                    <label
-                        className="label"
-                        style={
-                            item.checked
-                                ? { textDecoration: "line-through" }
-                                : null
-                        }
-                        onDoubleClick={() => handleCheck(item.id)}
-                    >
-                        {item.item}
-                    </label>
-                    <FaTrashAlt
-                        onClick={() => handleDelete(item.id)}
-                        role="button"
-                        tabIndex="0"
-                    />
-                </li>
+                <LineItems
+                    key={item.id}
+                    item={item}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ))}
         </ul>
     );
---
+++ 02App/src/components/LineItems.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItems = ({ item, handleCheck, handleDelete }) => {
+    return (
+        <li className="item" key={item.id}>
+            <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+            />
+            <label
+                className="label"
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+            >
+                {item.item}
+            </label>
+            <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+                aria-label={`Delete ${item.item}`}
+            />
+        </li>
+    );
+};
+
+export default LineItems;
